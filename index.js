ee6d7770af740299f64e479ba89c3d46ecd172c5
Charles Cortinas <crcortinas@gmail.com>
2019-03-13 20:14:09 -0500

    Added endpoint to change user password

--- index.js
+++ index.js
@@ -229,6 +229,45 @@ server.post("/api/login", (req, res) => {
   }
 });
 
+server.put("/api/passchange", protected, (req, res) => {
+  let creds = req.body;
+
+  if (!creds.username || !creds.oldPassword || !creds.newPassword) {
+    res
+      .status(400)
+      .json({ message: "Both username and old and new password required" });
+  } else if (creds.newPassword.length < 8) {
+    res
+      .status(400)
+      .json({ message: "New password must be at least 8 characters long" });
+  } else {
+    db("users")
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.oldPassword, user.password)) {
+          let newPass = bcrypt.hashSync(creds.newPassword, 8);
+          db("users")
+            .where({ username: user.username })
+            .update({ password: newPass })
+            .then(() => {
+              res.status(200).json({ message: "Password updated" });
+            })
+            .catch(err => {
+              console.log("inner", err);
+              res.status(500).json(err);
+            });
+        } else {
+          res.status(400).json({ message: "Incorrect username or password" });
+        }
+      })
+      .catch(err => {
+        console.log("outer", err);
+        res.status(500).json(err);
+      });
+  }
+});
+
 server.listen(server.get("port"), () => {
   console.log("== LISTENING ON PORT", server.get("port"), "==");
 });
